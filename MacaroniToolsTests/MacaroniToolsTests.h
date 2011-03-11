2a0a96b85c1847fdff0c0218ef03f0673e32e03f
Johannes Ekberg <johannes@Morotsmanen.local>
2011-03-11 14:12:27 +0100

    Initial Commit

---
+++ MacaroniToolsTests/MacaroniToolsTests.h
@@ -0,0 +1,17 @@
+//
+//  MacaroniToolsTests.h
+//  MacaroniToolsTests
+//
+//  Created by Johannes Ekberg on 2011-03-11.
+//  Copyright 2011 MacaroniCode Software. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface MacaroniToolsTests : SenTestCase {
+@private
+    
+}
+
+@end
